43cb2cb35202c9a1ea40e3877c64e510b6163f15
Cabriter <dreamcabriter@gmail.com>
2022-03-17 23:19:36 +0800

    添加自动补全

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -17,6 +17,20 @@ packer.startup({
     use("arkav/lualine-lsp-progress")
     -- telescope （新增）
     use { 'nvim-telescope/telescope.nvim', requires = { "nvim-lua/plenary.nvim" } }
+      -- nvim-cmp
+    use 'hrsh7th/cmp-nvim-lsp' -- { name = nvim_lsp }
+    use 'hrsh7th/cmp-buffer'   -- { name = 'buffer' },
+    use 'hrsh7th/cmp-path'     -- { name = 'path' }
+    use 'hrsh7th/cmp-cmdline'  -- { name = 'cmdline' }
+    use 'hrsh7th/nvim-cmp'
+    -- vsnip
+    use 'hrsh7th/cmp-vsnip'    -- { name = 'vsnip' }
+    use 'hrsh7th/vim-vsnip'
+    use 'rafamadriz/friendly-snippets'
+    -- lspkind
+    use 'onsails/lspkind-nvim'
+
+
   end,
   config = {
       display = {
