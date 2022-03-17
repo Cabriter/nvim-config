337d703cdfa8bd2cdfd3ede280e9f2b3faa21c78
Cabriter <dreamcabriter@gmail.com>
2022-03-17 19:18:06 +0800

    nvim-tree忽略unity .meta文件

--- lua/plugin-config/nvim-tree.lua
+++ lua/plugin-config/nvim-tree.lua
@@ -23,7 +23,7 @@ nvim_tree.setup({
   -- 隐藏 .文件 和 node_modules 文件夹
   filters = {
     dotfiles = true,
-    custom = { "node_modules" },
+    custom = { "node_modules" , "*.meta" },
   },
   view = {
     -- 宽度
